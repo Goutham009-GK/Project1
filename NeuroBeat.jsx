a7b15ddf6ea9bf679cac4c77189489d2eaaa633b
Goutham009-GK <goutham00967@gmail.com>
2026-04-05 12:29:07 +0530

    Create NeuroBeat.jsx file

---
+++ NeuroBeat.jsx
@@ -0,0 +1,12 @@
+// React component for NeuroBeat
+import React from 'react';
+
+const NeuroBeat = () => {
+    return (
+        <div>
+            <h1>Welcome to NeuroBeat</h1>
+        </div>
+    );
+};
+
+export default NeuroBeat;
