f50e93d443b4bb72a751c9134362c8b56a23796d
Goutham009-GK <goutham00967@gmail.com>
2026-04-05 12:29:31 +0530

    Update NeuroBeat.jsx with the complete NeuroBeat component code

--- NeuroBeat.jsx
+++ NeuroBeat.jsx
@@ -1,10 +1,10 @@
-// React component for NeuroBeat
 import React from 'react';
 
 const NeuroBeat = () => {
     return (
         <div>
             <h1>Welcome to NeuroBeat</h1>
+            <p>Your source for mental wellness.</p>
         </div>
     );
 };
